4d8700e48400fd8ae8b9e812aea9aca69579252e
tomasrzdevgold <141885719+tomasrzdevgold@users.noreply.github.com>
2023-11-23 00:45:12 -0300

    Update teste.js

--- pages/api/teste.js
+++ pages/api/teste.js
@@ -30,3 +30,26 @@ export default async function handler(request, response) {
     
    
 }*/
+
+
+import { sql } from '@vercel/postgres';
+
+
+
+export default async function handler(request, response) {
+
+  try {
+      const result = await sql`SELECT * FROM usuarios;`;
+      
+
+     return response.status(200).json({result})
+
+
+
+    } 
+    catch (error) {
+      return response.status(500).json({ error });
+    }
+    
+  
+  }
