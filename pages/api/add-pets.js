e82b4526721662008dbd1571189db4deaf7a7941
tomasrzdevgold <141885719+tomasrzdevgold@users.noreply.github.com>
2023-11-23 00:31:20 -0300

    Update add-pets.js

--- pages/api/add-pets.js
+++ pages/api/add-pets.js
@@ -53,7 +53,7 @@ export default async function handler(request, response) {
 */
 
 
-
+/* ///////////////
 export default async function handler(request, response) {
   try {
     const Nome = request.body.petName
@@ -78,3 +78,107 @@ export default async function handler(request, response) {
   </html>`
   return response.status(200).send(pet)
 }
+
+*/
+
+
+const erro1 = []
+
+  
+
+    const campos = {
+      nome: false,
+      email: false,
+      genero: false,
+      estados: false
+      }
+      
+      
+      
+      const expressoes = {
+      nome : /^[a-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑA-Z]{2,}\s[a-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑA-Z]{2,}$/ ,
+      email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+      }
+
+
+    export default async function handler(request, response) {
+      const Nome = request.body.nome
+      const Email = request.body.email
+      const Genero = request.body.genero 
+      const Estado = request.body.estados
+      const Cidade = request.body.cidade
+
+      if(expressoes.nome.test(Nome)){
+        campos["nome"] = true
+      }else{
+        campos["nome"] = false
+        erro1.push( `<p> O campo [Nome] deve ser preenchido corretamente </p>`)
+      }
+
+      if(expressoes.email.test(Email)){
+        campos["email"] = true
+      } else{
+        campos["email"] = false
+        erro1.push(`<p> O campo [Email] deve ter um email valido </p>`)
+      }
+      if(Genero === "F"){
+        campos["genero"] = true
+      }else if (Genero === "M") {
+        campos["genero"] = true
+      } else {
+        campos["genero"] =false
+        erro1.push( `<p> O campo [Genero] deve ser preenchido </p>`)
+      }
+
+      if(Estado === "0"){
+        campos["estados"] = false
+        erro1.push( `<p> O campo [Estado] deve ser preenchido </p>`)
+      }else {
+        campos["estados"] = true
+      }
+
+      if(campos.nome && campos.email && campos.genero && campos.estados){
+        try {
+          //if (!Nome || !Email || !Genero || !Estado) throw new Error('Pet and owner names required');
+          await sql`INSERT INTO usuarios (Nome, Email, Genero, Estado, Cidade, Receber_contato) VALUES (${Nome}, ${Email}, ${Genero} , ${Estado}, ${Cidade}, ${request.body.contato});`;
+        } catch (error) {
+          return response.status(500).json({ error });
+        }
+        
+        const succeso = `<!DOCTYPE html>
+        <body>
+            <h1>Parabens, ${Nome}, seu cadastro foi realizado com sucesso! </h1>
+            <p>Nome: ${Nome}</p>
+            <p>Email: ${Email} </p>
+            <p>Genero: ${Genero} </p>
+            <p>Localidade: ${Cidade}, ${Estado} </p>
+            <a href="http://127.0.0.1:5500/index.html">Pagina que lista os cadastro</a>
+        </body>
+        </html>`
+        
+        return response.status(200).send(succeso)
+      }else{
+        let erro = `
+        <h1> Ops, encontramos algum problema! </h1>
+        <a href="http://127.0.0.1:5500/index.html">Voltar para o formulario</a>
+
+        `
+        for (let i = 0; i < erro1.length; i++) {
+          erro += erro1[i];
+          
+        }
+        response.status(200).send(erro)
+
+       
+        }
+        for (let i=0; i <erro1.length;){
+          erro1.pop()
+        }
+
+    }
+
+
+
+
+
+
